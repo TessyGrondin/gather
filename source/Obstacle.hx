4a6d47cd6a18b5494b7f07130c99776b15b3a0a8
Tessy Grondin <tessy.grondin@epitech.eu>
2024-08-07 11:44:38 +0400

    collision points in obstacles

--- source/Obstacle.hx
+++ source/Obstacle.hx
@@ -4,16 +4,32 @@ import flixel.FlxSprite;
 
 class Obstacle extends FlxSprite
 {
+	var px1 = [23, 0, 21, 26, 44, 63];
+	var px2 = [40, 63, 42, 37, 18, 0];
+	var px3 = [70, 87, 91, 93, 51, 12, 2, 6, 50];
+	var px:Array<Int>;
+
+	var py1 = [0, 17, 51, 79, 93, 73];
+	var py2 = [0, 0, 8, 21, 47, 59, 51, 30, 16];
+	var py:Array<Int>;
+
 	public function new(relX:Int, relY:Int, type:Int, reversed:Bool)
 	{
 		super(relX * 32, relY * 32);
 
-        if (type == 1)
+        if (type == 1) {
         	loadGraphic(AssetPaths.other_obstacle__png, false, 64, 96);
-        else
+			py = py1;
+			px = px1;
+		} else {
             loadGraphic(AssetPaths.obstacle__png, false, 96, 64);
-        if (reversed)
+			py = py2;
+			px = px3;
+		}
+        if (reversed) {
+			px = px2;
             flipX = true;
+		}
 		this.x = relX * 32;
 		this.y = relY * 32;
 		updateHitbox();
